099f0fa9b34a0eae801c745725924820e571f7fe
nkitsan <nastik.kitsan@gmail.com>
2018-10-14 16:07:57 +0300

    Added: number and password validation Fixed: pic position

--- client/js/regform.js
+++ client/js/regform.js
@@ -1,11 +1,11 @@
-document.querySelector(".user-pic-input").addEventListener('change', updatePicture);;
-console.log("Event added");
+document.querySelector(".user-pic-input").addEventListener("change", updatePicture);
+
 
 function updatePicture() {
     let input = document.querySelector(".user-pic-input");
     let img = document.querySelector(".user-pic");
     let files = input.files;
-    if (files.length !==0 && (files[0].type === 'image/jpeg' || files[0].type === 'image/png')){
+    if (files.length !== 0){
         img.src = URL.createObjectURL(files[0]);
     }
 }
